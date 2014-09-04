82b6d9473cd3ed11f41470eb0856d710354fdfb3
Adam Mckaig <adam.mckaig@gmail.com>
2014-09-03 23:41:59 -0400

    Refactor Object3d into Segment
    I expected that Segment would be a tiny wrapper around Object, but it's
    actually pretty different. It's entirely possible that I'm just not very
    good at this, though, so maybe it'll come back later.
    
    This provides a nicer API, anyway.

--- euler_angles.go
+++ euler_angles.go
@@ -1,6 +1,9 @@
 package math3d
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
 
 type EulerAngles struct {
 	Heading float64 // x
@@ -12,10 +15,19 @@ var (
 	IdentityOrientation = &EulerAngles{}
 )
 
+func Euler(h float64, p float64, b float64) *EulerAngles {
+  return &EulerAngles{rad(h), rad(p), rad(b)}
+}
+
+// TODO: GTFO
 func MakeEulerAngles(h float64, p float64, b float64) *EulerAngles {
 	return &EulerAngles{h, p, b}
 }
 
 func (ea *EulerAngles) String() string {
 	return fmt.Sprintf("&Euler{h=%.2f p=%.2f b=%.2f}", ea.Heading, ea.Pitch, ea.Bank)
+}
+
+func rad(degrees float64) float64 {
+  return (math.Pi/180) * degrees
 }
